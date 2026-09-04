d324119dfc9e25d0bd0b020719c338ff4b8b4cd0
PoralisMeguki132 <poralismeguki@gmail.com>
2026-09-03 21:58:28 -0300

    SALVA PELO AMOR DE DEUS

--- public/scripts/cardapio.js
+++ public/scripts/cardapio.js
@@ -1,4 +1,21 @@
 async function carregarCardapio(){
 
-    //Pegar os dados no banco
+    let cardapio;
+
+    try {
+        const chamada = await fetch("http://localhost:8080/produtos");
+        if (!chamada.ok){
+            throw new Error("Response status: ${chamada.status}");
+        }
+
+        cardapio = await chamada.json();
+    } catch(error){
+        console.log("Houve um erro ao buscar os dados, tente novamente mais tarde.");
+        return;
+    }
+
+    let carpioHTML = "";
+    cardapio.forEach((item) -> {
+        cardapioHTML
+    }
 }
